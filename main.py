3c04cf3e069d6a1041a908538d6d3f388a21c051
Kanghean <kangheanhor@gmail.com>
2026-05-11 21:47:00 +1000

    Clean the code up into classes and different files for readability

---
+++ main.py
@@ -0,0 +1,13 @@
+from src.tracker import SpeedTest
+import schedule
+import time
+import datetime
+
+test = SpeedTest('data', 'tracker.csv')
+
+print(datetime.datetime.now().strftime("%H:%M:%S"))
+schedule.every().minute.at(":00").do(test.run_once)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
---
+++ src/tracker.py
@@ -0,0 +1,60 @@
+import speedtest
+import datetime
+import csv
+from pathlib import Path
+
+class SpeedTest:
+
+    def __init__(self, path_name, file_name):
+        self.path_name = Path(path_name)
+        self.file_name = file_name
+        self.headers = ["Test time", "Upload speed", "Download speed"]
+        
+    def measure(self):
+        current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        print(current_time)
+
+        wifi = speedtest.Speedtest()
+        wifi.get_best_server()
+
+        print("Start finding upload")
+        upload = wifi.upload()
+
+        print("Start finding download")
+        download = wifi.download()
+
+        upload_mbps = self.change_to_mbps(upload_mbps)
+        download_mbps = self.change_to_mbps(download_mbps)
+
+        print(f"The upload speed is {upload_mbps:.2f}")
+        print(f"The download speed is {download_mbps:.2f}")
+
+        return {self.headers[0] : current_time,
+                self.headers[1] : upload_mbps,
+                self.headers[2] : download_mbps}
+    
+    @staticmethod
+    def change_to_mbps(number):
+        return number / (1024 * 1024)
+    
+    def save_data(self, results):
+
+        #makes sure there is a folder
+        self.path_name.mkdir(parents=True, exist_ok=True)
+        file_location = self.path_name / self.file_name
+
+        #checks if the file exists 
+        need_headers = not file_location.exists() or file_location.stat().st_size == 0
+
+        with open(file_location, mode = 'a', newline="") as csvfile:
+
+            writer = csv.DictWriter(csvfile, fieldnames=self.headers)
+            if need_headers:
+                writer.writeheader()
+
+            writer.writerow(results)
+
+    def run_once(self):
+        results = self.measure()
+        self.save_data(results)
+        print("Data added to csv")
